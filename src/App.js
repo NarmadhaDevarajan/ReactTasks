923e8252f8d4eacde426e94e9641cfb9423876b8
NarmadhaDevarajan <narmi.jan4@gmail.com>
2023-07-05 09:05:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import "./App.css";
-import Home from "./Components/Home";
-import { useState } from "react";
-import Cart from "./Components/Cart";
-import Navigation from "./Components/Nav";
-import Header from './Components/Header'
-import productdata from "./Data/data";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import './App.css'
+import './Dashborad'
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addItem = (item) => {
-    const getData = productdata.filter((inp) => inp.id === item);
-    setCartItems([...cartItems, ...getData]);
-  };
-
-  const removeItem = (item) => {
-    console.log("remove item", item);
-    const filteredData = cartItems.filter((obj) => obj.id !== item);
-    console.log(filteredData);
-    setCartItems([...filteredData]);
-  };
   return (
-    <>
-      
-      <Navigation cartItems={cartItems} />
-      <Header />
-      <Cart cartItems={cartItems} removeItem={removeItem} />
-      <Home productdata={productdata} addItem={addItem} cartItems={cartItems} />
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
